e03517f3b3d853d764392e6ac7fbe859a97304b2
Leo AQ <taquoc196@gmail.com>
2024-07-12 08:36:33 +0700

    commit code todo list and code example HTML DOM Events

--- main.js
+++ main.js
@@ -11,34 +11,34 @@
 
 // var student = new Student('Anh', 'Quoc');
 
-// console.log(student.firstName); 
+// console.log(student.firstName);
 // console.log(student.lastName);
 // console.log(student.getFullName());
 
 // const Users = [
 //     {
-//         id: 1, 
+//         id: 1,
 //         name: 'Quoc',
 //         age: 20,
 
 //     },
 //     {
-//         id: 2, 
+//         id: 2,
 //         name: 'LEO',
 //         age: 22,
-        
+
 //     },
 //     {
-//         id: 3, 
+//         id: 3,
 //         name: 'LEOAQ',
 //         age: 11,
-        
+
 //     },
 //     {
-//         id: 4, 
+//         id: 4,
 //         name: 'LEOAQqqeqw',
 //         age: 28,
-        
+
 //     }
 // ];
 // const newUser = Users.map(user => {
@@ -61,7 +61,6 @@
 // console.log('getElementsByClassName', getElementsByClassName);
 // console.log(buttonElements);
 
-
 // var heading = document.querySelector("h1");
 // heading.title = "heading";
 // heading.setAttribute("data-title", "Heading");
@@ -77,19 +76,18 @@
 // coursesList.innerHTML = coursesHTML.join('');
 
 // //DOM CSS
-// coursesList.style.backgroundColor = '#f2f2f2'; 
-// coursesList.style.borderRadius = '5px'; 
+// coursesList.style.backgroundColor = '#f2f2f2';
+// coursesList.style.borderRadius = '5px';
 // coursesList.style.padding = '10px'; (
 
-function showPopup() {
-  document.getElementById("myPopup").style.display = "block"
-}
+// function showPopup() {
+//   document.getElementById("myPopup").style.display = "block"
+// }
 
-function hidePopup() {
-  document.getElementById("myPopup").style.display = "none";
+// function hidePopup() {
+//   document.getElementById("myPopup").style.display = "none";
 
- 
-}
+// }
 function addItem() {
   const inputValue = document.getElementById("myInput").value;
   const submitBtn = document.getElementById("subId");
@@ -109,4 +107,36 @@ function addItem() {
       hidePopup();
   }
 }
+function changeHeading() {
+  const h1 = document.querySelectorAll("h1");
+  h1[h1.length - 1].innerText = "Heading 4";
+  h1.item(1).innerText = "Heading 4"
+}
+const p = document.querySelector("p");
+p.addEventListener("mouseup", changeHeading);
+// O day neu de changeHeading() thi ngay lap tuc se lang nghe su kien mouseover. Nen chi can truyen tham chieu den ham changeHeading
 
+
+// document.querySelector('p').addEventListener("mouseover", function() {
+//   const getHeading = document.querySelectorAll('h1');
+//   getHeading[getHeading.length - 1].innerText = 'Heading1';
+
+// })
+document.querySelector("input").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    const getHeading = document.querySelectorAll("h1");
+    getHeading[getHeading.length - 1].innerText = "Heading 5 ";
+  }
+});
+document.querySelector("input").addEventListener("change", function () {
+  const getHeading = document.querySelectorAll("h1");
+  if (getHeading.length > 0) {
+    getHeading[getHeading.length - 1].innerText = "Heading 8";
+  }
+});
+document.querySelector("input").addEventListener("input", function () {
+  const getHeading = document.querySelectorAll("h1");
+  if (getHeading.length > 0) {
+    getHeading[getHeading.length - 1].innerText = "Heading 10";
+  }
+});
